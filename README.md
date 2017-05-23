6dfb0734fa695728746954a39381720e5f1b454b
weirdpattern <patricio@weirdpattern.com>
2017-05-23 00:50:40 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ayu Mirage Hyper Theme
+Ayu (mirage) for Hyper
+
+![Ayu Mirage Hyper Theme]()
+
+## Installation
+1. Open Hyper's preferences with `Cmd +`, (or manually at `~/.hyper.js`).
+2. Update your list of plugins to include `hyper-ayu-miragae`, like this:
+
+```javascript
+  plugins: [
+    'hyper-ayu-mirage'
+  ]
+```
+
+3. Fully reload Hyper (`Cmd + Shift + R`)
+
+## Configuration
+
+
+## Related
+
+- [Hyper](https://hyper.is/)
+- [Ayu for Sublime Text](https://github.com/dempfi/ayu)
+- [Ayu for Vim](https://github.com/ayu-theme/ayu-vim)
+- [Ayu (dark) for Hyper](https://github.com/licatajustin/hyper-ayu)
+- [Ayu (light) for Hyper](https://github.com/weirdpattern/hyper-ayu-light)
+
+## License
+MIT © WeirdPattern
---
+++ index.js
@@ -0,0 +1,129 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 WeirdPattern
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+const BACKGROUND = '#212733';
+const FOREGROUND = '#D9D7CE';
+
+const RED = '#ff3333';
+const GREEN = '#BAE67E';
+const YELLOW = '#FFD580';
+const BLUE = '#5CCFE6';
+const MAGENTA = '#D4BFFF';
+const LIGHT_GRAY = '#3D4752';
+const DARK_GRAY = '#3E4B59';
+const WHITE = '#FFFFFF';
+
+const CURSOR_COLOR = YELLOW;
+const BORDER_COLOR = BACKGROUND;
+
+let TAB_BORDER_COLOR = '#343D4A';
+const TAB_TEXT_COLOR = FOREGROUND;
+const TAB_BORDER_ACTIVE_COLOR = '#FFAE57';
+
+const colors = {
+  black: BACKGROUND,
+  red: RED,
+  green: GREEN,
+  yellow: YELLOW,
+  blue: BLUE,
+  magenta: MAGENTA,
+  cyan: BLUE,
+  white: LIGHT_GRAY,
+  lightBlack: DARK_GRAY,
+  lightRed: RED,
+  lightGreen: GREEN,
+  lightYellow: YELLOW,
+  lightBlue: BLUE,
+  lightMagenta: MAGENTA,
+  lightCyan: BLUE,
+  colorCubes: WHITE,
+  grayscale: FOREGROUND
+};
+
+exports.decorateConfig = (config) => {
+  let windowControlsCSS;
+
+  if (config.showWindowControls) {
+    windowControlsCSS = '.list_2902 { margin-left: 0 !important; }';
+  }
+
+  if (config.ayu && config.ayu.noBorder) {
+    TAB_BORDER_COLOR = BACKGROUND;
+  }
+
+  return Object.assign({}, config, {
+    foregroundColor: FOREGROUND,
+    backgroundColor: BACKGROUND,
+    borderColor: BORDER_COLOR,
+    cursorColor: CURSOR_COLOR,
+    colors,
+    css: `
+      ${config.css || ''}
+      .cursor-node {
+        background-color: ${MAGENTA} !important;
+        border-color: ${MAGENTA} !important;
+      }
+      .hyper_main {
+        border: none !important;
+      }
+      .header_header {
+        border-bottom: 1px solid ${TAB_BORDER_COLOR};
+        background: ${BACKGROUND} !important;
+      }
+      .splitpane_divider {
+        background-color: rgba(130, 128, 184, 0.5) !important;
+      }
+      .tabs_title {
+        color: ${TAB_TEXT_COLOR};
+        font-weight: 600;
+      }
+      .tab_tab {
+        border: 0;
+        border-left: 1px solid ${TAB_BORDER_COLOR} !important;
+        background-color: ${BACKGROUND};
+      }
+      .tab_text {
+        color: ${TAB_TEXT_COLOR};
+        font-weight: normal;
+      }
+      .tab_textActive {
+        color: ${TAB_TEXT_COLOR};
+        font-weight: 600;
+        background-color: ${BACKGROUND};
+        border-bottom: 1px solid ${TAB_BORDER_ACTIVE_COLOR} !important;
+      }
+      .tab_icon {
+        color: ${TAB_TEXT_COLOR};
+        font-weight: 600;
+      }
+      .tab_icon:hover {
+        color: ${TAB_TEXT_COLOR};
+        font-weight: 600;
+      }
+      ${windowControlsCSS}
+    `
+  })
+};
